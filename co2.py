8e448489bd13cda76033c8830e1bc7a35c04af9d
Manish <16449008+Manish22332@users.noreply.github.com>
2021-10-12 15:40:46 -0400

    update sensor codes

--- co2.py
+++ co2.py
@@ -1,37 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 8 11:47:39 2021
+
+@author: manish
+"""
+
 import RPi.GPIO as GPIO
 import time
 from scd30_i2c import SCD30
 from pushover import Client
+import sys
 # Initializing the SCD sensor
 scd30 = SCD30()
+# red led pin
+red_led = 16
 # client user_id and api_token for push notification
 client = Client("usuus8pcfq8b2knfc5b12f6ikgiosu", api_token="aapd3ssp5fzri8h6kma6x99p2jocgo")
-GPIO.setmode(GPIO.BCM)
-red_led = 16
-GPIO.setup(red_led, GPIO.OUT)
-scd30.set_measurement_interval(2)
-scd30.start_periodic_measurement()
 
-time.sleep(2)
+def main():
+    GPIO.setmode(GPIO.BCM)
+    
+    GPIO.setup(red_led, GPIO.OUT)
+    # set interval for data reading
+    scd30.set_measurement_interval()
+    scd30.start_periodic_measurement()
+    
+    time.sleep(2)
+    
+    while True:
+        # setting up the sensor to get readings
+        if scd30.get_data_ready():
+            data = scd30.read_measurement()
+            if data is not None:
+                print(f"CO2: {data[0]:.2f}ppm, temp: {data[1]:.2f}'C, rh: {data[2]:.2f}%")
+                # if co2 is greater than 2000 ppm or temperature is more than 26 degrees C
+                if data[0]>2000 or data[1]>26:
+                    GPIO.output(red_led, GPIO.HIGH)
+                else:  
+                    GPIO.output(red_led, GPIO.LOW)
+                    #client.send_message("Open the window", title="CO2 Poisoning notification")
+    
+            time.sleep(2)
+        else:
+            time.sleep(0.2)
+    
+    
+if __name__ == '__main__':
 
-while True:
-    if scd30.get_data_ready():
-        m = scd30.read_measurement()
-        if m is not None:
-            print(f"CO2: {m[0]:.2f}ppm, temp: {m[1]:.2f}'C, rh: {m[2]:.2f}%")
-            if m[0]>2000 or m[1]>26:
-                GPIO.output(red_led, GPIO.HIGH)
-                time.sleep(2)
-                GPIO.output(red_led, GPIO.LOW)
-                #client.send_message("Open the window", title="CO2 Poisoning notification")
-
-        time.sleep(2)
-    else:
-        time.sleep(0.2)
-
-# Code for push notification
-"""
-from pushover import Client
-client = Client("usuus8pcfq8b2knfc5b12f6ikgiosu", api_token="aapd3ssp5fzri8h6kma6x99p2jocgo")
-client.send_message("Open the window", title="CO2 Poisoning notification")
-"""
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Quitting')
+        GPIO.cleanup()
+        sys.exit(0)
--- new_motion_control.py
+++ new_motion_control.py
@@ -1,49 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 8 11:47:39 2021
+
+@author: manish
+"""
+
+# Initializing for motion sensor
 import RPi.GPIO as GPIO
 import time
-"""
-GPIO.setmode(GPIO.BCM)
-PIR_PIN = 4
-GPIO.setup(PIR_PIN, GPIO.IN)
-
-def MOTION(PIR_PIN):
-   print('Motion Detected!')
-
-print('PIR Module Test (CTRL+C to exit)')
-time.sleep(2)
-print('Ready')
-
-try:
-               GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=MOTION)
-               while 1:
-                              time.sleep(5)
-except KeyboardInterrupt:
-               print('Quit')
-               GPIO.cleanup()
-"""
-import time
+# Initializing for led
 import RPi.GPIO as GPIO1
+
+
 GPIO.setmode(GPIO.BCM)
+# Supress warnings
 GPIO.setwarnings(False)
+# Pin 4 for the sensor
 PIR_PIN = 4
-GPIO.setup(PIR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO1.setmode(GPIO1.BCM)
+# pin 21 for yellow led
 yellow_led=21
-GPIO1.setup(yellow_led, GPIO1.OUT)
-#GPIO.setup(PIR_PIN, GPIO.IN)
 
 
-print('Starting up the PIR Module (click on STOP to exit)')
-time.sleep(1)
-print ('Ready')
 
-while True:
-    #print GPIO.input(PIR_PIN)
-    #time.sleep(0.5)
-    if GPIO.input(PIR_PIN):
-        print('Motion Detected')
-	GPIO1.output(yellow_led, GPIO1.HIGH)
-        time.sleep(1)
-    else:
-        print('Motion Not Detected')
-	GPIO1.output(yellow_led, GPIO1.LOW)
-        time.sleep(1)
+def main():
+    
+    # Setup the motion sensor
+    GPIO.setup(PIR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    #Setup led
+    GPIO1.setmode(GPIO1.BCM)
+    
+    GPIO1.setup(yellow_led, GPIO1.OUT)
+    
+    
+    
+    print('Starting up the PIR Motion sensor')
+    time.sleep(1)                    
+    print('Ready to take the readings')
+                            
+    while True:             
+        #print GPIO.input(PIR_PIN)
+        #time.sleep(0.5)
+        if GPIO.input(PIR_PIN):
+            print('Motion Detected')
+            # If motion detected, switch on the led
+            GPIO1.output(yellow_led, GPIO1.HIGH)
+            time.sleep(2)
+        else:
+            print('Motion Not Detected')
+            # If there's no motion, switch off the led
+            GPIO1.output(yellow_led, GPIO1.LOW)
+            time.sleep(2)
+    
+    
+if __name__ == '__main__':
+
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Quitting')
+        GPIO.cleanup()
+        pass
+
+
+
+
+
+
+
+
+    
